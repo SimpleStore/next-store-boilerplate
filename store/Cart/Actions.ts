febffc2bba3dd6992e85053b86223469c4bae8d9
Nayab Khan <64783465+nayabkhn@users.noreply.github.com>
2020-07-17 15:37:52 +0500

    remove Item From Cart (#2)

--- store/Cart/Actions.ts
+++ store/Cart/Actions.ts
@@ -2,7 +2,7 @@ import * as Services from "../../lib/SimpleStore";
 
 export const CART_LOADED = "CART_LOADED";
 export const CART_ITEM_ADDED = "CART_ITEM_ADDED";
-
+export const CART_ITEM_REMOVED = "CART_ITEM_REMOVED";
 export const loadCart = async (cartId: string) => {
   return { type: CART_LOADED, payload: await Services.loadCart(cartId) };
 };
@@ -14,4 +14,8 @@ export const addToCart = async (
 ) => {
   const response = await Services.addToCart(cartId, productId, quantity);
   return { type: CART_ITEM_ADDED, payload: { ...response } };
+};
+export const removeFromCart = async (cartId: string, productId: string) => {
+  const response = await Services.removeFromCart(cartId, productId);
+  return { type: CART_ITEM_REMOVED, payload: { ...response } };
 };
--- store/Cart/Reducer.ts
+++ store/Cart/Reducer.ts
@@ -4,9 +4,10 @@ import * as Actions from "./Actions";
 export const cartReducer = (state: ICart, action) => {
   console.log("action: ", action);
   switch (action.type) {
-    
     case Actions.CART_LOADED:
     case Actions.CART_ITEM_ADDED:
+      return { ...action.payload };
+    case Actions.CART_ITEM_REMOVED:
       return { ...action.payload };
 
     default:
